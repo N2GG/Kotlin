e76a8649839c76249602a69a97b2d21313e95fff
N2GG <nol2ggoon2@gmail.com>
2023-10-23 18:51:23 +0900

    1

---
+++ app/src/main/java/com/example/kotlingrammer/Sample.kt
@@ -0,0 +1,17 @@
+package com.example.kotlingrammer
+
+fun main() {
+    helloworld()
+    helloworld()
+    omg()
+
+
+}
+
+fun helloworld () {
+    println("Hello Kotlin World")
+}
+
+fun omg () {
+    println("O M G")
+}
